645520ef298eac77ba953a688b944465dbcaa793
reyeroramses <reyeroramses@gmail.com>
2024-11-15 21:04:34 -0600

    update

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Cambia esto por tu usuario de MySQL
-    password: '4Q5@YJM$PHb#8dS%', // Cambia esto por tu contraseña de MySQL
-    database: 'movie_db' // Asegúrate de que esta base de datos exista
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql2');
 const peliculasRoutes = require('./routes/peliculas');
 const app = express();
 const PORT = 8080;
+require('dotenv').config(); // Cargar variables de entorno
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+// Configuración de la conexión a la base de datos
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// Conectar a la base de datos
+db.connect(err => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos MySQL');
+});
 
 // Rutas
 app.use('/peliculas', peliculasRoutes);
