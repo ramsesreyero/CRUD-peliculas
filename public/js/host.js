5f1d38d591ea44057b93a440c1e47978f8d956e9
reyeroramses <reyeroramses@gmail.com>
2024-11-21 07:10:12 -0600

    update

--- public/js/host.js
+++ public/js/host.js
@@ -1,2 +1,2 @@
-// export const host='http://10.22.2.91:8080';
-export const host='http://192.168.100.14:8080';
+export const host='http://10.22.2.91:8080';
+// export const host='http://192.168.100.14:8080';
