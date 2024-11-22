e0876a78ad438bbdd8672ef596dc0d9f3066f906
reyeroramses <reyeroramses@gmail.com>
2024-11-22 12:41:10 -0600

    update

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql2');
 require('dotenv').config(); // Asegúrate de que esto esté al principio
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST, // mysql.railway.internal
+    user: process.env.DB_USER, // root
+    password: process.env.DB_PASSWORD, // rvbevVIEJYqqzefyTimybsrWsWuplSrJ
+    database: process.env.DB_NAME // railway
 });
 
+// Conectar a la base de datos
 db.connect(err => {
     if (err) {
         console.error('Error conectando a la base de datos:', err);
@@ -16,4 +17,4 @@ db.connect(err => {
     console.log('Conectado a la base de datos MySQL');
 });
 
-module.exports = db;
+module.exports = db; // Asegúrate de exportar la conexión si es necesario
