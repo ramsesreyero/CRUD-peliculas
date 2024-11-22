9b921681ad5d803852966aa9f9c9c3ff814f149b
reyeroramses <reyeroramses@gmail.com>
2024-11-22 13:28:06 -0600

    update

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise'); // Usar mysql2/promise para facilitar e
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'rvbevVIEJYqqzefyTimybsrWsWuplSrJ',
+    database: process.env.DB_NAME || 'railway',
     port: process.env.DB_PORT || 3306, // Usa el puerto por defecto si no se especifica
     connectionLimit: 10 // Limitar el número de conexiones
 });
